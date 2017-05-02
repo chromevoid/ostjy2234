c3bb282664171f803e8f90385eb6e6b204ec289f
Chrome <chrome@127.179.233.68.naturalwireless.com>
2017-05-02 01:18:53 -0400

    created a sample app (Django)

---
+++ python-docs-samples-master/appengine/standard/django/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chrome/Documents/ostjy2234/python-docs-samples-master/appengine/standard/django/env/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
