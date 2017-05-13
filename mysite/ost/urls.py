f4ef53649b7a57073fc97025141622a535aaf1ae
Chrome <chromevoid500@gmail.com>
2017-05-13 18:51:12 -0400

    Update url.py

--- mysite/ost/urls.py
+++ mysite/ost/urls.py
@@ -12,6 +12,5 @@ urlpatterns = [
     url(r'^new/action$', views.create_resource, name='create_resource'),
     url(r'^resource/update/(?P<resource_id>\d+)$', views.update_resource, name='update_resource'),
     url(r'^resource/create/(?P<resource_id>\d+)$', views.create_reservation, name='create_reservation'),
-    url(r'^reservation/delete/(?P<reservation_id>\d+)$', views.delete_reservation, name='delete_reservation'),
-    url(r'^resource/feed/(?P<resource_id>\d+)$', views.ResourceReservationFeed(), name='resource_feed')
+    url(r'^reservation/delete/(?P<reservation_id>\d+)$', views.delete_reservation, name='delete_reservation')
 ]
