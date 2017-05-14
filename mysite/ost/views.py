924460c0808a7351a5c5a4263429e18883a84e5b
Chrome <chrome@127.179.233.68.naturalwireless.com>
2017-05-14 00:26:26 -0400

    added some comments

--- mysite/ost/views.py
+++ mysite/ost/views.py
@@ -14,10 +14,15 @@ import datetime
 
 
 def index(request, name_results=[], time_results=[]):
+    # get the reservations made by the current user
     my_reservation_list = Reservation.objects.filter(
         owner=users.get_current_user(),
     ).order_by('time')
+
+    # get the resources in the system
     resource_list = Resource.objects.all().order_by('-last')
+
+    # get the resources owned by the current user
     my_resource_list = Resource.objects.filter(
         owner=users.get_current_user(),
     )
@@ -40,6 +45,7 @@ def index(request, name_results=[], time_results=[]):
             end = hour * 60 + int(minute) + int(reservation.duration) * 60
             if current_time_in_minutes <= end:
                 pm_helper_list.append(reservation)
+
     return render(request, 'landing.html', {
         'my_reservation_list': my_reservation_list,
         'resource_list': resource_list,
@@ -54,16 +60,24 @@ def index(request, name_results=[], time_results=[]):
 
 
 def search_by_name(request):
+    # get the search name
     name = request.POST['search_by_name'].lower()
+
+    # get the resources in the system
     resource_list = Resource.objects.all().order_by('-last')
+
+    # check the name of each resource
+    # if the search name is a substring of the resource name, then add the resource to the result_list
     show_resource_list = []
     for r in resource_list:
         if r.name.lower().find(name) >= 0:
             show_resource_list.append(r)
+
     return index(request, show_resource_list, [])
 
 
 def search_by_time(request):
+    # get the fields, and convert HH:MM AM/PM to minutes
     search_duration = request.POST['duration']
     search_time, search_sign = request.POST['time'].split(" ")
     search_hour, search_minute = search_time.split(":")
@@ -74,8 +88,11 @@ def search_by_time(request):
     search_start = search_hour * 60 + int(search_minute)
     search_end = search_start + int(search_duration) * 60
 
+    # get the resources in the system
     resource_list = Resource.objects.all().order_by('-last')
-    resource_list_within_time = []
+
+    # check if the the start time and end time of a resource is within the search time period
+    # if true, then add the resource to the result_list
     resource_list_avaible = []
     for r in resource_list:
         time_start, sign_start = r.start.split(" ")
@@ -101,11 +118,16 @@ def search_by_time(request):
 
 
 def new(request):
+    # a page containing the form to create a new resource
     return render(request, 'new.html')
 
 
 def resource(request, resource_id=None):
+    # get current resource
     current_resource = get_object_or_404(Resource, pk=resource_id)
+
+    # check if the current resource is owned by the current user
+    # and get the tags string so that the tag value can be shown as default value in the form
     my_resource = Resource.objects.filter(
         owner=users.get_current_user(),
         pk=resource_id,
@@ -121,9 +143,12 @@ def resource(request, resource_id=None):
         for tag in tag_list:
             tag_list_string = tag_list_string + str(tag.name) + ", "
         tag_list_string = tag_list_string[:len(tag_list_string) - 2]
+
+    # get the reservations for current resource
     reservation_list = Reservation.objects.filter(
         resource=current_resource,
     )
+
     # to order the time correctly and ignore reservations that pass the time
     current = (datetime.datetime.now() + datetime.timedelta(hours=-4)).strftime('%H:%M')
     current_hour, current_minute = current.split(":")
@@ -143,6 +168,7 @@ def resource(request, resource_id=None):
             end = hour * 60 + int(minute) + int(reservation.duration) * 60
             if current_time_in_minutes <= end:
                 pm_helper_list.append(reservation)
+
     return render(request, 'resource.html', {
         'resource': current_resource,
         'tag_list_string': tag_list_string,
@@ -156,8 +182,10 @@ def resource(request, resource_id=None):
 
 
 def get_resource(request, tag_id=None):
+    # use a certain tag to find all the resources that have this tag
     current_tag = get_object_or_404(Tag, pk=tag_id)
     resource_list = current_tag.resource_set.all()
+
     return render(request, 'tag.html', {
         'current_tag': current_tag,
         'resource_list': resource_list,
@@ -165,12 +193,16 @@ def get_resource(request, tag_id=None):
 
 
 def get_user(request, username=None):
+    # fine the resources owned by a certain user
     resource_list = Resource.objects.filter(
         owner=username,
     )
+
+    # find the reservation owned by a certain user
     reservation_list = Reservation.objects.filter(
         owner=username,
     )
+
     # to order the time correctly and ignore reservations that pass the time
     current = (datetime.datetime.now() + datetime.timedelta(hours=-4)).strftime('%H:%M')
     current_hour, current_minute = current.split(":")
@@ -190,6 +222,7 @@ def get_user(request, username=None):
             end = hour * 60 + int(minute) + int(reservation.duration) * 60
             if current_time_in_minutes <= end:
                 pm_helper_list.append(reservation)
+
     return render(request, 'user.html', {
         'username': username,
         'resource_list': resource_list,
@@ -202,6 +235,7 @@ def get_user(request, username=None):
 
 
 def create_resource(request):
+    # get the information about the new resource
     new_resource = Resource()
     new_resource.created = datetime.datetime.now() + datetime.timedelta(hours=-4)
     new_resource.owner = users.get_current_user()
@@ -210,7 +244,8 @@ def create_resource(request):
     new_resource.end = request.POST['end']
     new_resource.description = request.POST['description']
     new_resource.last = datetime.datetime.now() + datetime.timedelta(hours=-4)
-    # check start and end time
+
+    # check start and end time, start time must be earlier than the end time
     time_start, sign_start = request.POST['start'].split(" ")
     time_end, sign_end = request.POST['end'].split(" ")
     hour_start, minute_start = time_start.split(":")
@@ -227,6 +262,8 @@ def create_resource(request):
         messages.error(request, "Create a resource: the start time should be before the end time.")
         return redirect(request.META.get('HTTP_REFERER'))
 
+    # deal with the tags
+    # if a tag doesn't exit in the Tag table, then a new tag is created before is added to the new resource
     if new_resource.name and new_resource.start and new_resource.end:
         new_resource.save()
         tags = request.POST['tags'].replace(" ", "").split(",")
@@ -243,16 +280,19 @@ def create_resource(request):
         messages.success(request, "Create a resource: success.")
     else:
         messages.error(request, "Create a resource: please enter the name, start time, and end time.")
+
     return redirect(request.META.get('HTTP_REFERER'))
 
 
 def update_resource(request, resource_id=None):
+    # get the information about the resource
     current_resource = get_object_or_404(Resource, pk=resource_id)
     current_resource.name = request.POST['name'].title()
     current_resource.start = request.POST['start']
     current_resource.end = request.POST['end']
     current_resource.description = request.POST['description']
-    # check start and end time
+
+    # check start and end time, start time should be earlier than the end time
     time_start, sign_start = request.POST['start'].split(" ")
     time_end, sign_end = request.POST['end'].split(" ")
     hour_start, minute_start = time_start.split(":")
@@ -269,6 +309,9 @@ def update_resource(request, resource_id=None):
         messages.error(request, "Update a resource: the start time should be before the end time.")
         return redirect(request.META.get('HTTP_REFERER'))
 
+    # check if the name, start time, and end time are entered since these fields are necessary
+    # if true, then deal with the tags
+    # if a tag doesn't exit in the Tag table, then a new tag is created before is added to the new resource
     if current_resource.name and current_resource.start and current_resource.end:
         current_resource.save()
         current_resource.tags.clear()
@@ -286,11 +329,15 @@ def update_resource(request, resource_id=None):
         messages.success(request, "Update a resource: success.")
     else:
         messages.error(request, "Update a resource: please enter the name, start time, and end time.")
+
     return redirect(request.META.get('HTTP_REFERER'))
 
 
 def create_reservation(request, resource_id=None):
+    # first, get the resource that the user is making reservation for
     current_resource = get_object_or_404(Resource, pk=resource_id)
+
+    # get the information of the reservation
     new_reservation = Reservation()
     new_reservation.created = datetime.datetime.now() + datetime.timedelta(hours=-4)
     new_reservation.owner = users.get_current_user()
@@ -298,6 +345,8 @@ def create_reservation(request, resource_id=None):
     new_reservation.time = request.POST['time']
     new_reservation.duration = int(request.POST['duration'])
 
+    # check if the duration is a integer greater than 0
+    # if a decimal is used, then the largest integer that is smaller than this decimal is used
     if new_reservation.duration <= 0:
         messages.error(request, "Make a reservation: duration must be greater than 0")
         return redirect(request.META.get('HTTP_REFERER'))
@@ -366,6 +415,9 @@ def create_reservation(request, resource_id=None):
             no_time = True
             break
 
+    # check if this reservation should be made
+    # if true, then the resource also needs to be updated: the last field and the reservation_count field
+    # if success, a mail is sent to confirm that the reservation is made.
     if compare_reservation_start < compare_resource_start or compare_reservation_end > compare_resource_end:
         messages.error(request, "Make a reservation: time is not within the available hours of the resource.")
     elif reserved:
@@ -385,13 +437,16 @@ def create_reservation(request, resource_id=None):
                        )
         current_resource.save()
         messages.success(request, "Make a reservation: success.")
+
     return redirect(request.META.get('HTTP_REFERER'))
 
 
 def delete_reservation(request, reservation_id=None):
+    # get the current reservation and then delete
     current_reservation = get_object_or_404(Reservation, pk=reservation_id)
     current_reservation.delete()
     messages.success(request, "Delete a reservation: success.")
+
     return redirect(request.META.get('HTTP_REFERER'))
 
 
